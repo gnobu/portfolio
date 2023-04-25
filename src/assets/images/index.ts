b4d85c95b377ea34bdb5a000374d3846d4878295
gnobu <inyangubong17@gmail.com>
2023-04-25 17:35:38 +0100

    Added tech stack icons

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,3 +1,14 @@
 import homeBg from "./homebg.png"
+import expressIcon from './expressjs.png'
+import javascriptIcon from './javascript.png'
+import mongodbIcon from './mongodb.png'
+import nodejsIcon from './nodejs.png'
+import reactjsIcon from './reactjs.png'
+import remixIcon from './remix.png'
+import typescriptIcon from './typescript.png'
 
-export { homeBg }
+export {
+    homeBg, expressIcon, javascriptIcon,
+    mongodbIcon, nodejsIcon, reactjsIcon,
+    remixIcon, typescriptIcon
+}
